5ff725284c14d0ae190a74fe72696e684c82d0ca
Nebi017 <wedizere123@gmail.com>
2025-03-22 14:32:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 async function connectDatabase() {
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_URL);
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
     console.log("Connected Succesfully");
   } catch (error) {
     console.error(error.message);
