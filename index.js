7a046caa2a2fc32c6b6582b82373138dfd8b5be0
Nebi017 <wedizere123@gmail.com>
2025-03-22 14:09:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const contactRoutes = require("./routes/contactRoutes.js")
 require("dotenv").config();
 
 const app = express();
+connectDatabase();
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
-connectDatabase();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
