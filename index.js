f7ba5747d7dd59bfca35108da686d872dbf5c4ce
Nebi017 <wedizere123@gmail.com>
2025-03-22 22:37:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.get("/", (request, response) => {
-  console.log(request);
-  return response.status(234).send("welcome to Mern Stack");
-});
+
 // POST route to create a new lost/found item
 app.use("/api/items", itemRoute);
 app.use("/api/contact", contactRoutes);
 app.use("/", userRoute);
-
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("welcome to Mern Stack");
+});
 
 const port = process.env.PORT || 3000 ;
 app.listen(port, () => {
